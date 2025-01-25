5a369b70bc6fe359f96bdc7713503672743669bb
Ibrahim-Hassan74 <ih3046376@gmail.com>
2025-01-25 20:12:22 +0200

    The partial view assignment

--- ViewsAssignment/ViewsAssignment/obj/Release/net6.0/ViewsAssignment.AssemblyInfo.cs
+++ ViewsAssignment/ViewsAssignment/obj/Release/net6.0/ViewsAssignment.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ViewsAssignment")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd812a15165c486e265db3b740c8ea2ba9e7a658")]
 [assembly: System.Reflection.AssemblyProductAttribute("ViewsAssignment")]
