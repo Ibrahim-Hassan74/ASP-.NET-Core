79421115bf87add08c427b8a8338842ebcfb48e6
Ibrahim-Hassan74 <ih3046376@gmail.com>
2025-02-05 01:16:34 +0200

    Configuration Assignment

---
+++ ConfigurationAssignment/ConfigurationAssignment/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using ConfigurationAssignment.Options;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace ConfigurationAssignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SocialMediaLinksOptions _socialMedialLinksOptions;
+
+        public HomeController(IOptions<SocialMediaLinksOptions> socialMedialLinksOptions)
+        {
+            _socialMedialLinksOptions = socialMedialLinksOptions.Value;
+        }
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(_socialMedialLinksOptions);
+        }
+    }
+}
